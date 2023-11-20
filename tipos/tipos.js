b39ecb648dcc279db5a20b76538b534bb41b019e
Ramis Damasceno <ramis.damasceno@youvisa.com.br>
2023-11-20 12:59:30 -0300

    enums

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -2,3 +2,35 @@
 //string
 let nome = 'João';
 console.log(nome);
+//numbers 
+let idade = 24;
+idade = 24.9;
+console.log(idade);
+//boolean
+let hobbies = true;
+console.log(hobbies);
+// hobbies = 2
+//tipos explicitos
+let minhaIdade;
+minhaIdade = 25;
+console.log(typeof minhaIdade);
+//array
+let atividades = ["Cozinhar", "Jogar"];
+console.log(atividades[0]);
+console.log(typeof atividades);
+atividades = [100, 200, 300];
+console.log(atividades);
+//tuplas 
+let endereço = ["Av. das Dores", 1260];
+console.log(endereço);
+//enums
+var Cor;
+(function (Cor) {
+    Cor[Cor["Azul"] = 0] = "Azul";
+    Cor[Cor["Amarelo"] = 1] = "Amarelo";
+    Cor[Cor["Vermelho"] = 2] = "Vermelho"; // 2
+})(Cor || (Cor = {}));
+let corPrimaria = Cor.Azul;
+console.log(corPrimaria);
+console.log(typeof Cor);
+console.log(typeof corPrimaria);
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -1,4 +1,44 @@
 //string
-
-let nome = 'João'
+let nome: string = 'João'
 console.log(nome)
+
+//numbers 
+let idade: number = 24
+idade = 24.9
+console.log(idade)
+
+//boolean
+let hobbies:boolean = true
+console.log(hobbies)
+// hobbies = 2
+
+//tipos explicitos
+let minhaIdade: number
+minhaIdade = 25
+console.log(typeof minhaIdade)
+
+//array
+
+let atividades: any[] = ["Cozinhar", "Jogar"]
+console.log(atividades[0])
+console.log(typeof atividades)
+
+atividades = [100, 200, 300]
+console.log(atividades)
+
+//tuplas 
+let endereço: [string, number] = ["Av. das Dores", 1260]
+console.log(endereço) 
+
+//enums
+
+enum Cor {
+    Azul, // 0
+    Amarelo, // 1
+    Vermelho // 2
+}
+
+let corPrimaria: Cor = Cor.Azul
+console.log(corPrimaria)
+console.log(typeof Cor)
+console.log(typeof corPrimaria)
